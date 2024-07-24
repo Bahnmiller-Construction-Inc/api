d155c7c60c3fa37a685e5df71d242076a729f531
Nate Walchenbach <nate@nwsolutions.io>
2024-07-24 11:31:15 -0700

    removed msal

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { ConfidentialClientApplication } = require("@azure/msal-node");
 const axios = require("axios");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -15,38 +14,7 @@ app.use(
   })
 );
 
-const msalConfig = {
-  auth: {
-    clientId: process.env.CLIENT_ID,
-    authority: process.env.AUTHORITY,
-    clientSecret: process.env.CLIENT_SECRET,
-  },
-};
-
-const cca = new ConfidentialClientApplication(msalConfig);
-
-app.get("/getToken/msal", async (req, res) => {
-  const clientCredentialRequest = {
-    scopes: ["https://graph.microsoft.com/.default"], // Request for Graph API scope
-  };
-
-  try {
-    const response = await cca.acquireTokenByClientCredential(
-      clientCredentialRequest
-    );
-    res.json({ accessToken: response.accessToken });
-  } catch (error) {
-    console.error("Error acquiring token:", error.message);
-    console.error(error.stack);
-    res.status(500).json({
-      message: "Error acquiring token",
-      error: error.message,
-      stack: error.stack,
-    });
-  }
-});
-
-app.get("/getToken/axios", async (req, res) => {
+app.get("/getToken", async (req, res) => {
   try {
     const response = await axios.post(
       `${process.env.AUTHORITY}/oauth2/v2.0/token`,
