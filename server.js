bca4b2339b07133ede9418b15a6d0b3d1d279677
Nate Walchenbach <nate@nwsolutions.io>
2024-07-24 14:35:54 -0700

    added new file

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ app.use(
   })
 );
 
+// Middleware to log requests
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 app.get("/getToken", async (req, res) => {
   try {
     const data = qs.stringify({
